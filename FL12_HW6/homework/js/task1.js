7cea172317fcb9fe677d4d34c0bee51f20181946
Ermondel <ermondel@gmail.com>
2019-12-19 20:58:37 +0200

    FL12_HW6: homework: task 1 done (quadratic equation)

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,32 @@
-// Your code goes here
+const a = parseInt(prompt('Enter A number', 1));
+const b = parseInt(prompt('Enter B number', 1));
+const c = parseInt(prompt('Enter C number', 1));
+
+if (isNaN(a + b + c)) {
+  console.log('Invalid input data');
+} else {
+  const DIGITS = 2;
+  const SQUARE = 2;
+  const FACTOR1 = 4;
+  const FACTOR2 = 2;
+
+  const discriminant = Math.pow(b, SQUARE) - FACTOR1 * a * c;
+
+  let x1;
+  let x2;
+
+  if (discriminant > 0) {
+    x1 = ((-b - Math.sqrt(discriminant)) / (FACTOR2 * a)).toFixed(DIGITS);
+    x2 = ((-b + Math.sqrt(discriminant)) / (FACTOR2 * a)).toFixed(DIGITS);
+  } else if (discriminant === 0) {
+    x1 = (-b / (FACTOR2 * a)).toFixed(DIGITS);
+  }
+
+  if (x1 && x2) {
+    console.log(`x1 = ${x1} and x2 = ${x2}`);
+  } else if (x1) {
+    console.log(`x = ${x1}`);
+  } else {
+    console.log(`no solution`);
+  }
+}
