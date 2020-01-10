7ad46c799385100e77e15f8840a3627e5d427cb1
Ermondel <ermondel@gmail.com>
2020-01-10 19:17:36 +0200

    FL12_HW9: homework: refactor: tasks 1, 2, 3

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,4 +1,6 @@
-// Task 1
+/**
+ * Task 1
+ */
 function convert() {
   const result = [];
   for (let i = 0; i < arguments.length; i++) {
@@ -12,60 +14,41 @@ function convert() {
   return result;
 }
 
-// console.log(convert('1', 2, 3, '4'));
+const a = '1';
+const b = 2;
+const c = 3;
+const d = '4';
+console.log(convert(a, b, c, d));
 
-// Additional tests
-/*
-(function test_convert() {
-  console.log('Test 00:', convert('1', 2, 3, '4'));
-  console.log('Test 01:', convert('1', '2', '3'));
-  console.log('Test 02:', convert(1, 2, 3));
-  console.log('Test 03:', convert(1.1, 2.2, 3.3));
-  console.log('Test 04:', convert('1.1', '2.2', '3.3'));
-  console.log('Test 05:', convert(1.1111, 2.2222, 3.3333));
-  console.log('Test 06:', convert('1.1111', '2.2222', '3.3333'));
-  console.log('Test 07:', convert([], [], '-100.01'));
-  console.log('Test 08:', convert([], [], -100.01));
-  console.log('Test 09:', convert([], []));
-  console.log('Test 10:', convert({}, {}));
-  console.log('Test 11:', convert());
-})();
-*/
-
-// Task 2
+/**
+ * Task 2
+ */
 function executeforEach(arr, callback) {
   arr.forEach(callback);
 }
 
-// executeforEach([1, 2, 3], function(el) {
-//   console.log(el * 2);
-// });
+const e = 1;
+const f = 2;
+const g = 3;
+const square = 2;
+executeforEach([e, f, g], function(el) {
+  console.log(el * square);
+});
 
-// Task 3
+/**
+ * Task 3
+ */
 function mapArray(arr, callback) {
   executeforEach(arr, callback);
   return arr;
 }
 
-// function mapArrayCallback(el, index, arr) {
-//   arr[index] = +arr[index] + 3;
-// }
-
-// Additional tests
-/*
-(function test_convert() {
-  console.log('Test 00:', mapArray([2, '5', 8], mapArrayCallback));
-  console.log('Test 01:', mapArray(['0', '1', '2', '3'], mapArrayCallback));
-  console.log('Test 02:', mapArray([0, 1, 2, 3], mapArrayCallback));
-  console.log('Test 03:', mapArray([0, -1, -2, -3, -4], mapArrayCallback));
-  console.log('Test 04:', mapArray(['0', '-1', '-2', '-3', '-4'], mapArrayCallback));
-  console.log('Test 05:', mapArray([0, -1.1, -2.2, -3.3, -4.4], mapArrayCallback));
-  console.log('Test 06:', mapArray(['0', '-1.1', '-2.2', '-3.3', '-4.4'], mapArrayCallback));
-  console.log('Test 07:', mapArray([0, 1.1111, 2.2222, 3.3333, 4.4444], mapArrayCallback));
-  console.log('Test 08:', mapArray(['0', '1.1111', '2.2222', '3.3333', '4.4444'], mapArrayCallback));
-  console.log('Test 09:', mapArray([0, -110.1111, -220.2222, -330.3333, -440.4444], mapArrayCallback));
-  console.log('Test 10:', mapArray(['0', '-110.1111', '-220.2222', '-330.3333', '-440.4444'], mapArrayCallback));
-  console.log('Test 11:', mapArray([' ', '0000', ' 0 ', '-', 'qwe'], mapArrayCallback));
-  console.log('Test 12:', mapArray([], mapArrayCallback));
-})();
-*/
+const h = 2;
+const i = '5';
+const j = 8;
+const cube = 3;
+console.log(
+  mapArray([h, i, j], function(el, index, arr) {
+    arr[index] = +arr[index] + cube;
+  })
+);
