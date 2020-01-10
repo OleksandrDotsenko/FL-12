af1eee343a4cc992a3900d267a8f61cd681a58d0
Ermondel <ermondel@gmail.com>
2020-01-10 17:56:56 +0200

    FL12_HW9: homework: add task 3 (mapArray)

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -40,3 +40,32 @@ function executeforEach(arr, callback) {
 // executeforEach([1, 2, 3], function(el) {
 //   console.log(el * 2);
 // });
+
+// Task 3
+function mapArray(arr, callback) {
+  executeforEach(arr, callback);
+  return arr;
+}
+
+// function mapArrayCallback(el, index, arr) {
+//   arr[index] = +arr[index] + 3;
+// }
+
+// Additional tests
+/*
+(function test_convert() {
+  console.log('Test 00:', mapArray([2, '5', 8], mapArrayCallback));
+  console.log('Test 01:', mapArray(['0', '1', '2', '3'], mapArrayCallback));
+  console.log('Test 02:', mapArray([0, 1, 2, 3], mapArrayCallback));
+  console.log('Test 03:', mapArray([0, -1, -2, -3, -4], mapArrayCallback));
+  console.log('Test 04:', mapArray(['0', '-1', '-2', '-3', '-4'], mapArrayCallback));
+  console.log('Test 05:', mapArray([0, -1.1, -2.2, -3.3, -4.4], mapArrayCallback));
+  console.log('Test 06:', mapArray(['0', '-1.1', '-2.2', '-3.3', '-4.4'], mapArrayCallback));
+  console.log('Test 07:', mapArray([0, 1.1111, 2.2222, 3.3333, 4.4444], mapArrayCallback));
+  console.log('Test 08:', mapArray(['0', '1.1111', '2.2222', '3.3333', '4.4444'], mapArrayCallback));
+  console.log('Test 09:', mapArray([0, -110.1111, -220.2222, -330.3333, -440.4444], mapArrayCallback));
+  console.log('Test 10:', mapArray(['0', '-110.1111', '-220.2222', '-330.3333', '-440.4444'], mapArrayCallback));
+  console.log('Test 11:', mapArray([' ', '0000', ' 0 ', '-', 'qwe'], mapArrayCallback));
+  console.log('Test 12:', mapArray([], mapArrayCallback));
+})();
+*/
