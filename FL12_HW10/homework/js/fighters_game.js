88a95c33955e8ee2db0e5213e90c9cad76787f71
Ermondel <ermondel@gmail.com>
2020-01-18 23:54:43 +0200

    FL12_HW10: homework: base battle implementation

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,6 +1,22 @@
 function Fighter({ name, damage, hp, strength, agility }) {
+  const MAX_POWER = 100;
+
   let wins = 0;
   let losses = 0;
+
+  const attackPower = () => Math.floor(Math.random() * MAX_POWER) + 1;
+
+  const hit = (enemy) => {
+    const defense = enemy.getStrength() + enemy.getAgility();
+
+    if (defense < MAX_POWER && attackPower() > defense) {
+      enemy.dealDamage(damage);
+
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   return {
     getName: () => name,
@@ -8,29 +24,54 @@ function Fighter({ name, damage, hp, strength, agility }) {
     getStrength: () => strength,
     getAgility: () => agility,
     getHealth: () => hp,
-    attack: () => 'attack action',
     logCombatHistory: () => console.log(`Name: ${name}, Wins: ${wins}, Losses: ${losses}`),
     heal: () => 'heal method',
-    dealDamage: () => 'damage action',
     addWin: () => ++wins,
-    addLoss: () => ++losses
+    addLoss: () => ++losses,
+    attack: (enemy) => {
+      if (hit(enemy)) {
+        console.log(`${name} makes ${damage} to ${enemy.getName()}`);
+      } else {
+        console.log(`${name} attack missed`);
+      }
+    },
+    dealDamage: (damage) => {
+      if (hp <= damage) {
+        hp = 0;
+      } else {
+        hp -= damage;
+      }
+    }
   };
 }
 
-const myFighter = new Fighter({ name: 'Maximus', damage: 25, hp: 100, strength: 30, agility: 25 });
+function battle(fighter1, fighter2) {
+  const hp1 = fighter1.getHealth;
+  const hp2 = fighter2.getHealth;
+
+  if (!hp1()) {
+    console.log(`${fighter1.getName()} is dead.`);
+  } else if (!hp2()) {
+    console.log(`${fighter2.getName()} is dead.`);
+  } else {
+    while (hp1() && hp2()) {
+      fighter1.attack(fighter2);
+
+      if (hp2()) {
+        fighter2.attack(fighter1);
+      }
+    }
+
+    if (hp1()) {
+      console.log(`${fighter1.getName()} has won!`);
+    } else {
+      console.log(`${fighter2.getName()} has won!`);
+    }
+  }
+}
+
+const myFighter1 = new Fighter({ name: 'Maximus', damage: 25, hp: 100, strength: 30, agility: 25 });
+const myFighter2 = new Fighter({ name: 'Drakonus', damage: 30, hp: 100, strength: 40, agility: 25 });
 
 // test
-console.log('getName:', myFighter.getName());
-console.log('getDamage:', myFighter.getDamage());
-console.log('getStrength:', myFighter.getStrength());
-console.log('getAgility:', myFighter.getAgility());
-console.log('getHealth:', myFighter.getHealth());
-console.log(myFighter.attack());
-console.log(myFighter.dealDamage());
-myFighter.logCombatHistory();
-console.log(myFighter.addWin());
-console.log(myFighter.addLoss());
-myFighter.logCombatHistory();
-console.log(myFighter.addWin());
-console.log(myFighter.addLoss());
-myFighter.logCombatHistory();
+battle(myFighter1, myFighter2);
