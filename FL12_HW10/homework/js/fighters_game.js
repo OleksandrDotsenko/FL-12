17cd949e51576ac1859e0b66fff7aa3ce14f11d7
Ermondel <ermondel@gmail.com>
2020-01-19 13:36:31 +0200

    FL12_HW10: homework: provide healing

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,5 +1,6 @@
 function Fighter({ name, damage, hp, strength, agility }) {
   const MAX_POWER = 100;
+  const TOTAL_HP = 100;
 
   let wins = 0;
   let losses = 0;
@@ -21,8 +22,6 @@ function Fighter({ name, damage, hp, strength, agility }) {
     getStrength: () => strength,
     getAgility: () => agility,
     getHealth: () => hp,
-    logCombatHistory: () => console.log(`Name: ${name}, Wins: ${wins}, Losses: ${losses}`),
-    heal: () => 'heal method',
     addWin: () => ++wins,
     addLoss: () => ++losses,
     attack: (enemy) => {
@@ -35,7 +34,12 @@ function Fighter({ name, damage, hp, strength, agility }) {
     },
     dealDamage: (damage) => {
       hp = damage >= hp ? 0 : hp - damage;
-    }
+    },
+    heal: (amount) => {
+      const sum = hp + amount;
+      hp = sum > TOTAL_HP ? TOTAL_HP : sum;
+    },
+    logCombatHistory: () => console.log(`Name: ${name}, Wins: ${wins}, Losses: ${losses}`)
   };
 }
 
@@ -78,6 +82,16 @@ const myFighter1 = new Fighter({ name: 'Maximus', damage: 25, hp: 100, strength:
 const myFighter2 = new Fighter({ name: 'Drakonus', damage: 30, hp: 100, strength: 40, agility: 25 });
 
 battle(myFighter1, myFighter2);
+
+// test
 // console.log('\n');
 // myFighter1.logCombatHistory();
 // myFighter2.logCombatHistory();
+// console.log('\n');
+// console.log(`${myFighter1.getName()} hp: ${myFighter1.getHealth()}`);
+// console.log(`${myFighter2.getName()} hp: ${myFighter2.getHealth()}`);
+// myFighter1.heal(1000);
+// myFighter2.heal(1000);
+// console.log('\n');
+// console.log(`${myFighter1.getName()} hp: ${myFighter1.getHealth()}`);
+// console.log(`${myFighter2.getName()} hp: ${myFighter2.getHealth()}`);
