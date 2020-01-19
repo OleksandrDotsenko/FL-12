fb0c702350663dc7fd96633ac4c84f8aa2ac3e1e
Ermondel <ermondel@gmail.com>
2020-01-19 17:27:16 +0200

    FL12_HW10: homework: refactoring the fighter function

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -5,15 +5,32 @@ function Fighter({ name, damage, hp, strength, agility }) {
   let wins = 0;
   let losses = 0;
 
-  const generateAttack = () => Math.floor(Math.random() * MAX_POWER) + 1;
+  const _generateAttack = () => Math.floor(Math.random() * MAX_POWER) + 1;
 
-  const checkAttack = (enemy) => {
+  const attack = (enemy) => {
     const defense = enemy.getStrength() + enemy.getAgility();
-    const isAttack = defense < MAX_POWER && generateAttack() > defense;
+    const isAttack = defense < MAX_POWER && _generateAttack() > defense;
+
     if (isAttack) {
       enemy.dealDamage(damage);
+      console.log(`${name} makes ${damage} damage to ${enemy.getName()}`);
+      // console.log(`${name} makes ${damage} damage to ${enemy.getName()}`, `[${name} ${hp} : ${enemy.getName()} ${enemy.getHealth()}]`);
+    } else {
+      console.log(`${name} attack missed`);
     }
-    return isAttack;
+  };
+
+  const dealDamage = (damage) => {
+    hp = damage >= hp ? 0 : hp - damage;
+  };
+
+  const heal = (amount) => {
+    const sum = hp + amount;
+    hp = sum > TOTAL_HP ? TOTAL_HP : sum;
+  };
+
+  const logCombatHistory = () => {
+    console.log(`Name: ${name}, Wins: ${wins}, Losses: ${losses}`);
   };
 
   return {
@@ -24,22 +41,10 @@ function Fighter({ name, damage, hp, strength, agility }) {
     getHealth: () => hp,
     addWin: () => ++wins,
     addLoss: () => ++losses,
-    attack: (enemy) => {
-      if (checkAttack(enemy)) {
-        console.log(`${name} makes ${damage} to ${enemy.getName()}`);
-        // console.log(`${name} makes ${damage} to ${enemy.getName()}`, `[${name} ${hp} : ${enemy.getName()} ${enemy.getHealth()}]`);
-      } else {
-        console.log(`${name} attack missed`);
-      }
-    },
-    dealDamage: (damage) => {
-      hp = damage >= hp ? 0 : hp - damage;
-    },
-    heal: (amount) => {
-      const sum = hp + amount;
-      hp = sum > TOTAL_HP ? TOTAL_HP : sum;
-    },
-    logCombatHistory: () => console.log(`Name: ${name}, Wins: ${wins}, Losses: ${losses}`)
+    logCombatHistory,
+    dealDamage,
+    attack,
+    heal
   };
 }
 
@@ -82,16 +87,3 @@ const myFighter1 = new Fighter({ name: 'Maximus', damage: 25, hp: 100, strength:
 const myFighter2 = new Fighter({ name: 'Drakonus', damage: 30, hp: 100, strength: 40, agility: 25 });
 
 battle(myFighter1, myFighter2);
-
-// test
-// console.log('\n');
-// myFighter1.logCombatHistory();
-// myFighter2.logCombatHistory();
-// console.log('\n');
-// console.log(`${myFighter1.getName()} hp: ${myFighter1.getHealth()}`);
-// console.log(`${myFighter2.getName()} hp: ${myFighter2.getHealth()}`);
-// myFighter1.heal(1000);
-// myFighter2.heal(1000);
-// console.log('\n');
-// console.log(`${myFighter1.getName()} hp: ${myFighter1.getHealth()}`);
-// console.log(`${myFighter2.getName()} hp: ${myFighter2.getHealth()}`);
