af4fb234fd05062a47a6aeafd704654dbae4c528
Ermondel <ermondel@gmail.com>
2020-01-23 14:35:13 +0200

    FL12_HW11: homework: add style

--- FL12_HW11/homework/app.js
+++ FL12_HW11/homework/app.js
@@ -38,24 +38,42 @@ const structure = [
 const rootNode = document.getElementById('root');
 
 const options = {
-  titleTag: 'div',
+  rootTag: 'ul',
+  branchTag: 'li',
+  titleTag: 'span',
+  iconTag: 'i',
   titleClass: 'el-title',
   openClass: 'open',
-  closeClass: 'close'
+  closeClass: 'close',
+  classFolder: 'folder',
+  classFile: 'file',
+  iconSet: 'material-icons',
+  iconFolderOpen: 'folder_open',
+  iconFolderClose: 'folder',
+  iconFile: 'insert_drive_file'
 };
 
 function createElementsTreeByStructure(structure) {
-  const rootElement = document.createElement('ul');
+  const rootElement = document.createElement(options.rootTag);
 
   for (const elem of structure) {
     if (elem.title) {
-      const branchElement = document.createElement('li');
+      const branchElement = document.createElement(options.branchTag);
+      const branchIcon = document.createElement(options.iconTag);
       const branchTitle = document.createElement(options.titleTag);
+
       const branchTitleText = document.createTextNode(elem.title);
+      const branchIconText = document.createTextNode(elem.folder ? options.iconFolderClose : options.iconFile);
+
+      const elementType = elem.folder ? options.classFolder : options.classFile;
+
       branchElement.setAttribute('class', options.closeClass);
       branchTitle.setAttribute('class', options.titleClass);
+      branchIcon.setAttribute('class', `${options.iconSet} ${elementType}`);
 
+      branchIcon.appendChild(branchIconText);
       branchTitle.appendChild(branchTitleText);
+      branchElement.appendChild(branchIcon);
       branchElement.appendChild(branchTitle);
 
       if (elem.folder && elem.children) {
@@ -72,15 +90,30 @@ function createElementsTreeByStructure(structure) {
 
 function onFileTreeClick(event) {
   const element = event.target.parentElement;
-  let className = element.getAttribute('class');
+  const className = element.getAttribute('class');
+
   if (className) {
     if (className.indexOf(options.openClass) >= 0) {
       const newClass = className.replace(options.openClass, options.closeClass);
       element.setAttribute('class', newClass);
+      const icon = element.firstElementChild;
+      if (icon) {
+        const iconClass = icon.getAttribute('class');
+        if (iconClass && iconClass.indexOf(options.classFolder) >= 0) {
+          icon.textContent = options.iconFolderClose;
+        }
+      }
     }
     if (className.indexOf(options.closeClass) >= 0) {
       const newClass = className.replace(options.closeClass, options.openClass);
       element.setAttribute('class', newClass);
+      const icon = element.firstElementChild;
+      if (icon) {
+        const iconClass = icon.getAttribute('class');
+        if (iconClass && iconClass.indexOf(options.classFolder) >= 0) {
+          icon.textContent = options.iconFolderOpen;
+        }
+      }
     }
   }
 }
