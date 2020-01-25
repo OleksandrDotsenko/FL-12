77dae6c0349251c46bc338a74b800fa39a0138bd
Ermondel <ermondel@gmail.com>
2020-01-26 00:34:36 +0200

    FL12_HW12: homework: add rerender and form extension

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -1,4 +1,3 @@
-// Router
 class Router {
   constructor(routes) {
     this.routes = routes;
@@ -66,7 +65,6 @@ class Router {
   }
 }
 
-// Pages
 class Page {
   constructor(className, headerText) {
     this.className = className;
@@ -105,8 +103,16 @@ class MainPage extends Page {
 }
 
 class AddPage extends Page {
-  constructor() {
+  constructor(rerender) {
     super('AddPage', 'Add new item');
+    this.rerender = rerender;
+    this.onFormClick = this.onFormClick.bind(this);
+    this.data = {
+      newsetname: '',
+      counter: 0,
+      terms: [],
+      definitions: []
+    };
   }
 
   onFormSubmit(event) {
@@ -114,18 +120,53 @@ class AddPage extends Page {
   }
 
   onFormClick(event) {
-    console.log(event.target.name);
+    const isAdd = event.target.name === 'add';
+    // const isSave = event.target.name === 'save';
+
+    if (isAdd) {
+      const form = document.getElementById('addform');
+
+      const newsetname = form.elements['newsetname'];
+      const terms = form.elements['terms[]'];
+      const definitions = form.elements['definitions[]'];
+
+      this.data.newsetname = newsetname.value;
+
+      if (terms && definitions) {
+        const newTerms = [];
+        const newDefinitions = [];
+
+        if (!terms.length) {
+          newTerms.push(terms.value);
+          newDefinitions.push(definitions.value);
+        } else {
+          for (let i = 0; i < terms.length; i++) {
+            newTerms.push(terms[i].value);
+            newDefinitions.push(definitions[i].value);
+          }
+        }
+
+        this.data.terms = newTerms;
+        this.data.definitions = newDefinitions;
+      }
+
+      this.data.counter = this.data.counter + 1;
+
+      this.rerender();
+    }
   }
 
   content() {
     const form = document.createElement('form');
     form.setAttribute('name', 'addform');
+    form.setAttribute('id', 'addform');
     form.addEventListener('submit', this.onFormSubmit);
     form.addEventListener('click', this.onFormClick);
 
     const inputName = document.createElement('input');
     inputName.setAttribute('type', 'text');
-    inputName.setAttribute('name', 'item');
+    inputName.setAttribute('name', 'newsetname');
+    inputName.setAttribute('value', this.data.newsetname ? this.data.newsetname : '');
     inputName.setAttribute('placeholder', 'name');
     form.appendChild(inputName);
 
@@ -151,6 +192,27 @@ class AddPage extends Page {
     buttonsBlock.appendChild(btnCancel);
 
     form.appendChild(buttonsBlock);
+
+    for (let i = 0; i < this.data.counter; i++) {
+      const dl = document.createElement('div');
+      dl.setAttribute('class', 'card');
+
+      const dt = document.createElement('input');
+      dt.setAttribute('type', 'text');
+      dt.setAttribute('name', 'terms[]');
+      dt.setAttribute('value', this.data.terms[i] ? this.data.terms[i] : '');
+      dt.setAttribute('placeholder', 'Enter term');
+
+      const dd = document.createElement('input');
+      dd.setAttribute('type', 'text');
+      dd.setAttribute('name', 'definitions[]');
+      dd.setAttribute('value', this.data.definitions[i] ? this.data.definitions[i] : '');
+      dd.setAttribute('placeholder', 'Enter definition');
+
+      dl.appendChild(dt);
+      dl.appendChild(dd);
+      form.appendChild(dl);
+    }
 
     return this.generate(form);
   }
@@ -182,7 +244,6 @@ class NotFoundPage extends Page {
   }
 }
 
-// App
 class App {
   constructor(root) {
     const routes = [
@@ -210,7 +271,9 @@ class App {
 
     this.root = root;
     this.router = new Router(routes);
+    this.currentPage = null;
 
+    this.rerender = this.rerender.bind(this);
     window.addEventListener('hashchange', this.handlerHashchange.bind(this));
   }
 
@@ -223,14 +286,15 @@ class App {
     if (route) {
       document.title = 'My App :: ' + route.title;
       if (!route.Page.content) {
-        route.Page = new route.Page();
+        route.Page = new route.Page(this.rerender);
       }
-
-      this.content(route.Page.content());
+      this.currentPage = route.Page;
+      this.rerender();
     }
   }
 
-  content(newNode) {
+  rerender() {
+    const newNode = this.currentPage.content();
     if (!this.root.firstChild) {
       this.root.appendChild(newNode);
     } else {
